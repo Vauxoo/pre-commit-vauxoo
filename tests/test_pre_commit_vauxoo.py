85f58dd7ae347633301d7bb5858e7d4bf6cefc14
Moisés López <moylop260@vauxoo.com>
2022-08-22 03:35:07 -0500

    [REF] tests: Migrating tests to unittest

--- tests/test_pre_commit_vauxoo.py
+++ tests/test_pre_commit_vauxoo.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 from contextlib import contextmanager
 
 from click.testing import CliRunner
@@ -16,47 +17,48 @@ def chdir(path):
         os.chdir(curdir)
 
 
-def test_basic():
-    runner = CliRunner()
-    env = os.environ
-    env['INCLUDE_LINT'] = 'resources'
-    env['PRECOMMIT_AUTOFIX'] = '1'
-    result = runner.invoke(main, [], env=env)
-
-    assert result.output == ''
-    assert result.exit_code == 0
-
-
-def test_chdir():
-    runner = CliRunner()
-    env = os.environ
-    env['PRECOMMIT_AUTOFIX'] = '1'
-    with chdir("resources"):
+class TestStringMethods(unittest.TestCase):
+    def test_basic(self):
+        runner = CliRunner()
+        env = os.environ
+        env['INCLUDE_LINT'] = 'resources'
+        env['PRECOMMIT_AUTOFIX'] = '1'
         result = runner.invoke(main, [], env=env)
+        self.assertEqual(result.output, '')
+        self.assertEqual(result.exit_code, 0)
 
-    assert result.output == ''
-    assert result.exit_code == 0
+    def test_chdir(self):
+        runner = CliRunner()
+        env = os.environ
+        env['PRECOMMIT_AUTOFIX'] = '1'
+        with chdir("resources"):
+            result = runner.invoke(main, [], env=env)
+
+        self.assertEqual(result.output, '')
+        self.assertEqual(result.exit_code, 0)
+
+    def test_exclude_lint(self):
+        runner = CliRunner()
+        env = os.environ
+        env['PRECOMMIT_AUTOFIX'] = '1'
+        env['EXCLUDE_LINT'] = 'import-error'
+        with chdir("resources"):
+            result = runner.invoke(main, [], env=env)
+
+        self.assertEqual(result.output, '')
+        self.assertEqual(result.exit_code, 0)
+
+    def test_exclude_autofix(self):
+        runner = CliRunner()
+        env = os.environ
+        env['PRECOMMIT_AUTOFIX'] = '1'
+        env['EXCLUDE_AUTOFIX'] = 'resources/module_example1/demo/'
+        with chdir("resources"):
+            result = runner.invoke(main, [], env=env)
+
+        self.assertEqual(result.output, '')
+        self.assertEqual(result.exit_code, 0)
 
 
-def test_exclude_lint():
-    runner = CliRunner()
-    env = os.environ
-    env['PRECOMMIT_AUTOFIX'] = '1'
-    env['EXCLUDE_LINT'] = 'import-error'
-    with chdir("resources"):
-        result = runner.invoke(main, [], env=env)
-
-    assert result.output == ''
-    assert result.exit_code == 0
-
-
-def test_exclude_autofix():
-    runner = CliRunner()
-    env = os.environ
-    env['PRECOMMIT_AUTOFIX'] = '1'
-    env['EXCLUDE_AUTOFIX'] = 'resources/module_example1/demo/'
-    with chdir("resources"):
-        result = runner.invoke(main, [], env=env)
-
-    assert result.output == ''
-    assert result.exit_code == 0
+if __name__ == '__main__':
+    unittest.main()
