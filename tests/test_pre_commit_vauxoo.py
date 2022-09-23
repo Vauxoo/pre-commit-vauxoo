75a9dcc912a721d204f6f468b507b42c2bc2ca18
antonag32 <antonio@vauxoo.com>
2022-09-23 13:07:06 -0500

    [REF] pre_commit_vauxoo: Test repo structure set to standards The previous structure was: /tmp_dir/resources/all_modules
    Which is not standard. New structure is:
    /tmp_dir/all_modules

--- tests/test_pre_commit_vauxoo.py
+++ tests/test_pre_commit_vauxoo.py
@@ -20,16 +20,15 @@ class TestPreCommitVauxoo(unittest.TestCase):
         self.tmp_dir = tempfile.mkdtemp(suffix="_pre_commit_vauxoo")
         os.chdir(self.tmp_dir)
         self.runner = CliRunner()
-        src_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "resources")
+        src_path = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), "resources")
         self.create_dummy_repo(src_path, self.tmp_dir)
         self.maxDiff = None
-        os.environ["EXCLUDE_AUTOFIX"] = "resources/module_autofix1"
+        os.environ["EXCLUDE_AUTOFIX"] = "module_autofix1"
 
     def create_dummy_repo(self, src_path, dest_path):
         subprocess.call(["git", "init", "--initial-branch=main", dest_path])
-        dest_subpath = os.path.join(dest_path, os.path.basename(src_path))
-        shutil.copytree(src_path, dest_subpath)
-        subprocess.call(["git", "add", dest_subpath])
+        subprocess.call("cp %s %s -rf" % (os.path.join(src_path, "*"), dest_path), shell=True)
+        subprocess.call(["git", "add", dest_path])
 
     def tearDown(self):
         super().tearDown()
@@ -58,7 +57,7 @@ class TestPreCommitVauxoo(unittest.TestCase):
             yield
 
     def test_basic(self):
-        os.environ["INCLUDE_LINT"] = "resources"
+        os.environ["INCLUDE_LINT"] = self.tmp_dir
         os.environ["PRECOMMIT_HOOKS_TYPE"] = "all"
         result = self.runner.invoke(main, [])
         self.assertEqual(result.exit_code, 0, "Exited with error %s" % result.output)
@@ -68,18 +67,19 @@ class TestPreCommitVauxoo(unittest.TestCase):
     def test_chdir(self):
         self.runner = CliRunner()
         os.environ["PRECOMMIT_HOOKS_TYPE"] = "all"
-        os.chdir("resources")
-        expected_logs = ["WARNING:pre-commit-vauxoo:Running in current directory 'resources'"]
+
+        os.chdir("module_autofix1")
+        expected_logs = ["WARNING:pre-commit-vauxoo:Running in current directory '%s'" % "module_autofix1"]
         with self.custom_assert_logs("pre-commit-vauxoo", level="WARNING", expected_logs=expected_logs):
             result = self.runner.invoke(main, [])
         self.assertEqual(result.exit_code, 0, "Exited with error %s" % result.output)
 
     def test_exclude_lint_path(self):
         self.runner = CliRunner()
-        os.chdir("resources")
+
         os.environ["PRECOMMIT_HOOKS_TYPE"] = "all"
         os.environ["BLACK_SKIP_STRING_NORMALIZATION"] = "false"
-        os.environ["EXCLUDE_LINT"] = "resources/module_example1/models"
+        os.environ["EXCLUDE_LINT"] = "module_example1/models"
         result = self.runner.invoke(main, [])
         self.assertEqual(result.exit_code, 0, "Exited with error %s" % result.output)
         with open(os.path.join(self.tmp_dir, "pyproject.toml"), "r") as f_pyproject:
@@ -93,9 +93,9 @@ class TestPreCommitVauxoo(unittest.TestCase):
 
     def test_exclude_autofix(self):
         self.runner = CliRunner()
-        os.chdir("resources")
+
         os.environ["PRECOMMIT_HOOKS_TYPE"] = "all"
-        os.environ["EXCLUDE_AUTOFIX"] = "resources/module_example1/demo/"
+        os.environ["EXCLUDE_AUTOFIX"] = "module_example1/demo/"
         os.environ["BLACK_SKIP_STRING_NORMALIZATION"] = "true"
         result = self.runner.invoke(main, [])
         self.assertEqual(result.exit_code, 0, "Exited with error %s" % result.output)
@@ -152,7 +152,7 @@ class TestPreCommitVauxoo(unittest.TestCase):
 
     def test_uninstallable(self):
         os.environ["PRECOMMIT_HOOKS_TYPE"] = "all"
-        uninstallable_path = os.path.join(self.tmp_dir, "resources/module_uninstallable")
+        uninstallable_path = os.path.join(self.tmp_dir, "module_uninstallable")
         result = self.runner.invoke(main, ["-p", uninstallable_path])
 
         self.assertEqual(
