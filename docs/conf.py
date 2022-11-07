9bb51f0d4f6733a8fc39c9211553cb05fbd5b0f9
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2022-11-06 20:11:02 -0600

    Bump version: 7.0.1 → 7.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = "pre-commit-vauxoo"
 year = "2022"
 author = "Vauxoo"
 copyright = "{}, {}".format(year, author)
-version = release = "7.0.1"
+version = release = "7.0.2"
 
 pygments_style = "trac"
 templates_path = ["."]
