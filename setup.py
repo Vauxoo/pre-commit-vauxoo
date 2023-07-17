34489f02585922b6f0c9c16568c9a185c4370ffa
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2023-07-17 13:41:42 -0600

    Bump version: 7.0.18 → 7.0.19

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def read(*names, **kwargs):
 
 setup(
     name="pre-commit-vauxoo",
-    version="7.0.18",
+    version="7.0.19",
     license="LGPL-3.0-or-later",
     description="pre-commit script to run automatically the configuration and variables custom from Vauxoo",
     long_description_content_type="text/x-rst",
