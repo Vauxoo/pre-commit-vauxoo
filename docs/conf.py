42781dd77fb86c7d302bcde223cd33eb707e13db
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2024-11-04 13:42:45 -0600

    Bump version: 8.1.1 → 8.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ project = "pre-commit-vauxoo"
 year = "2022"
 author = "Vauxoo"
 copyright = "{}, {}".format(year, author)
-version = release = "8.1.1"
+version = release = "8.1.2"
 
 pygments_style = "trac"
 templates_path = ["."]
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def read(*names, **kwargs):
 
 setup(
     name="pre-commit-vauxoo",
-    version="8.1.1",
+    version="8.1.2",
     license="LGPL-3.0-or-later",
     description="pre-commit script to run automatically the configuration and variables custom from Vauxoo",
     long_description_content_type="text/x-rst",
