87c77744bcae4cc49a9a73cb19474c18aa3bc21f
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2025-12-26 23:31:25 -0600

    [FIX] pre-commit-config-autofix: Fix prettier to autofix xml files

---
+++ src/pre_commit_vauxoo/cfg/.oca_hooks_prettier.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('prettier').Config} */
+
+const config = {
+  // https://github.com/prettier/prettier/issues/15388#issuecomment-1717746872
+  plugins: [require.resolve("@prettier/plugin-xml")],
+  bracketSpacing: false,
+  printWidth: 119,
+  proseWrap: "always",
+  semi: true,
+  trailingComma: "es5",
+  xmlWhitespaceSensitivity: "preserve",
+  xmlSelfClosingSpace: true,
+};
+
+module.exports = config;
