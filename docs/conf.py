3f234370e0dd2522fc548c6320e764ce1979c42d
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2026-08-10 13:32:58 -0600

    Bump version: 8.3.1 → 8.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ project = "pre-commit-vauxoo"
 year = "2022"
 author = "Vauxoo"
 copyright = f"{year}, {author}"
-version = release = "8.3.1"
+version = release = "8.3.2"
 
 pygments_style = "trac"
 templates_path = ["."]
